30fe702c0c7e6f421ea4a358ffb7cf3155add57b
Connor Hull <chull4704@gmail.com>
2026-05-06 20:25:44 -0400

    Routing skeleton for plans page

--- routes/plans.js
+++ routes/plans.js
@@ -1,8 +1,48 @@
 import { Router } from 'express';
 const router = Router();
 
-router.get('/', async (req, res) => {
-  res.render('planner', { title: 'Planner' });
-});
+router.route('/') // main page
+  .get(async (req, res) => {
+    // would like this to render an empty calender for the current day on the frontend 
+    res.render('planner', { title: 'Planner' });
+  })
+  .post(async (req, res) => {
+    // this will give the user the ability to create a new plan
+  });
+
+router.get('/all', async (req, res) => {
+  // this will retrieve all of the saved plans belonging to the logged-in user
+})
+
+router.route('/all/:planId') // plan specific page
+  .get(async (req, res) => {
+    // this will retrieve a plan specified by the user on the frontend 
+  })
+  .put('/all/:planId', async (req, res) => {
+    // update the time, change date, etc. of a pre-existing plan
+  })
+  .delete('/all/:planId', async (req, res) => {
+    // delete a plan
+  })
+
+router.route('/all/:planId/activities')
+  .get((req, res) => {
+    // this will retrive all of the activities/locations of a specific plan
+  })
+  .post((req, res) => {
+    // add an activity to a plan
+  })
+
+router.route('all/:planId/activities/:activityId')
+  .put((req, res) => {
+    // update a plan and its parameters
+  })
+  .delete((req, res) => {
+    // delete a specific activity
+  })
+
+router.get('all?date=YYYY-MM-DD', (req, res) => {
+  // this will filter plans by specific day
+})
 
 export default router;
