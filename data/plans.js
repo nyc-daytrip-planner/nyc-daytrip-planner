0009cf648f2c4049040ea70ac98306b5266194c8
Connor Hull <chull4704@gmail.com>
2026-05-06 23:07:40 -0400

    POST method data functions and checkTime helper

--- data/plans.js
+++ data/plans.js
@@ -45,5 +45,71 @@ const exportedMethods = {
     if (!plan) throw "Error: Plan not found"
 
     return plan
+  },
+
+  async newPlan(userId, title, date, isPublic = false, locations = []) {
+    userId = checkId(userId)
+    title = checkString(title)
+    date = checkDate(date)
+
+    if (typeof isPublic != 'boolean') throw "Error: must be boolean"
+
+    let activities = []
+
+    if (locations.length > 0) {
+      activities = locations.map(({ locationId, startTime, endTime, notes = null }) => ({
+        locationId: new ObjectId(checkId(locationId)),
+        startTime: startTime || null,
+        endTime: endTime || null,
+        notes
+      }))
+    }
+
+    const newPlan = {
+      userId: new ObjectId(userId),
+      title,
+      date,
+      status: 'active',
+      isPublic,
+      activities,
+      photos: [],
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }
+
+    const planCollection = await plans()
+    const newInsert = await planCollection.insertOne(newPlan)
+    const newId = newInsert.insertedId
+    return await this.getPlanById(newId.toString())
+  },
+
+  async addActivity(planId, locationId, startTime, endTime, notes = "") {
+    planId = checkId(planId)
+    locationId = checkId(locationId)
+    startTime = checkTime(startTime)
+    endTime = checkTime(endTime)
+
+    if (typeof notes != 'string') throw "Error: notes must be of type string"
+
+    const newActivity = {
+      locationId: new ObjectId(locationId),
+      startTime,
+      endTime,
+      notes
+    }
+
+    const planCollection = await plans()
+    const result = await planCollection.updateOne(
+      { _id: new ObjectId(planId) },
+      {
+        $push: { activities: newActivity },
+        $set: { updatedAt: new Date() }
+      }
+    )
+
+    if (result.modifiedCount === 0) throw "Error: could not add activity"
+
+    return await this.getPlanById(planId)
   }
+
 }
--- routes/plans.js
+++ routes/plans.js
@@ -2,23 +2,23 @@ import { Router } from 'express';
 const router = Router();
 
 router.route('/') // main page
-  .get(async (req, res) => {
+  .get(async (req, res) => { // DONE
     // would like this to render an empty calender for the current day on the frontend 
     res.render('planner', {
       title: 'Planner',
       date: new Date().toISOString().split('T')[0] // passes date so that frontend knows current date
     });
   })
-  .post(async (req, res) => {
-    // this will give the user the ability to create a new plan
+  .post(async (req, res) => { // DONE
+    // this will give the user the ability to create a new plan 
   });
 
-router.get('/all', async (req, res) => {
-  // this will retrieve all of the saved plans belonging to the logged-in user
+router.get('/all', async (req, res) => { // DONE
+  // this will retrieve all of the saved plans belonging to the logged-in user 
 })
 
 router.route('/all/:planId') // plan specific page
-  .get(async (req, res) => {
+  .get(async (req, res) => { // DONE
     // this will retrieve a plan specified by the user on the frontend 
   })
   .put('/all/:planId', async (req, res) => {
@@ -29,10 +29,10 @@ router.route('/all/:planId') // plan specific page
   })
 
 router.route('/all/:planId/activities')
-  .get((req, res) => {
+  .get((req, res) => { // DONE
     // this will retrive all of the activities/locations of a specific plan
   })
-  .post((req, res) => {
+  .post((req, res) => { // DONE
     // add an activity to a plan
   })
 
@@ -44,8 +44,8 @@ router.route('all/:planId/activities/:activityId')
     // delete a specific activity
   })
 
-router.get('all?date=YYYY-MM-DD', (req, res) => {
-  // this will filter plans by specific day
+router.get('all?date=YYYY-MM-DD', (req, res) => { // DONE
+  // this will filter plans by specific day 
 })
 
 export default router;
