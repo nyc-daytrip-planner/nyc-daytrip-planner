60cf363832291a030e6244f5df96d15c08388b42
Alanzo1 <114414363+Alanzo1@users.noreply.github.com>
2026-05-07 22:20:59 -0400

    added cookie parser

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import session from 'express-session';
+import cookieParser from 'cookie-parser';
 import { engine } from 'express-handlebars';
 import configRoutes from './routes/index.js';
 
@@ -19,6 +20,7 @@ app.set('view engine', 'handlebars');
 app.set('views', './views');
 
 // middleware
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
@@ -35,6 +37,39 @@ app.use(session({
 // makes session user available in every handlebars template
 app.use((req, res, next) => {
   res.locals.user = req.session.user;
+  next();
+});
+
+let totalRequests = 0;
+app.use((req, res, next) => {
+  totalRequests++;
+
+  console.log('The request has all the following cookies:');
+  console.log(req.cookies);
+
+  if (req.cookies.lastAccessed) {
+    console.log('This user last accessed the site at ' + req.cookies.lastAccessed);
+  } else {
+    console.log('This user has never accessed the site before');
+  }
+
+  if (totalRequests % 5 === 0) {
+    const anHourAgo = new Date();
+    anHourAgo.setHours(anHourAgo.getHours() - 1);
+    res.cookie('lastAccessed', '', { expires: anHourAgo, httpOnly: true });
+    res.clearCookie('lastAccessed');
+    return next();
+  }
+
+  const now = new Date();
+  const expiresAt = new Date();
+  expiresAt.setHours(expiresAt.getHours() + 1);
+  res.cookie('lastAccessed', now.toString(), {
+    expires: expiresAt,
+    httpOnly: true,
+    sameSite: 'lax'
+  });
+
   next();
 });
 
